4031970ccd3562cadac6de01c5c1ca29f88ea3b3
CodeKiro <kirofiniello@outlook.it>
2024-07-02 17:41:50 +0200

    creazione dello scaffolding, e creazione dell'array di array con username e password

---
+++ index.php
@@ -0,0 +1,28 @@
+<?php 
+$users = [
+    [
+        'username' => 'user1',
+        'password' => 'password1'
+    ],
+    [
+        'username' => 'user2',
+        'password' => 'password2'
+    ],
+    [
+        'username' => 'user3',
+        'password' => 'password3'
+    ]
+];
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
